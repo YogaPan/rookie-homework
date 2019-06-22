4abe87293268602b0ebc6db885e87c8c23a3245d
user <galtz0321@gmail.com>
2019-06-22 23:00:08 +0800

    加入基本樣式和排版

--- HW2/src/App.jsx
+++ HW2/src/App.jsx
@@ -7,24 +7,38 @@ import { updateResult, clearResult } from './actions';
 
 const App = ({ result, clearResult }) => {
   return (
-    <div className="app">
-      <p>{result}</p>
-      <button type="button" onClick={() => clearResult()}>AC</button>
+    <div className="App">
+      <p className="result">{result}</p>
+      <div className="button-row">
+        <button className="calc-button calc-button--functional" type="button" onClick={() => clearResult()}>AC</button>
+        <CalcButton buttonNumber="+/-" isFunctional={true} />
+        <CalcButton buttonNumber="%" isFunctional={true}/>
+        <CalcButton buttonNumber="/" isOperator={true} />
+      </div>
 
       <div className="button-row">
         <CalcButton buttonNumber="7" />
         <CalcButton buttonNumber="8" />
         <CalcButton buttonNumber="9" />
+        <CalcButton buttonNumber="*" isOperator={true} />
       </div>
       <div className="button-row">
         <CalcButton buttonNumber="4" />
         <CalcButton buttonNumber="5" />
         <CalcButton buttonNumber="6" />
+        <CalcButton buttonNumber="-" isOperator={true}/>
       </div>
       <div className="button-row">
         <CalcButton buttonNumber="1" />
         <CalcButton buttonNumber="2" />
         <CalcButton buttonNumber="3" />
+        <CalcButton buttonNumber="+" isOperator={true}/>
+      </div>
+      <div className="button-row">
+        <CalcButton buttonNumber="0" />
+        <CalcButton buttonNumber="." />
+        <CalcButton buttonNumber="." />
+        <CalcButton buttonNumber="=" isOperator={true}/>
       </div>
     </div>
   );
--- HW2/src/components/CalcButton.jsx
+++ HW2/src/components/CalcButton.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { updateResult } from '../actions';
 
-const CalcButton = ({ buttonNumber, updateResult }) => {
+const CalcButton = ({ buttonNumber, isOperator, isFunctional, updateResult }) => {
+  let className = 'calc-button';
+  if (isOperator) className += ' calc-button--operator';
+  if (isFunctional) className += ' calc-button--functional';
+
   return (
     <button
+      className={className}
       type="button"
       onClick={() => updateResult(Number(buttonNumber))}
     >
@@ -14,8 +19,15 @@ const CalcButton = ({ buttonNumber, updateResult }) => {
   );
 };
 
+CalcButton.defaultProps = {
+  isOperator: false,
+  isFunctional: false,
+};
+
 CalcButton.propTypes = {
   buttonNumber: PropTypes.string.isRequired,
+  isOperator: PropTypes.bool,
+  isFunctional: PropTypes.bool,
   updateResult: PropTypes.func.isRequired,
 };
 
